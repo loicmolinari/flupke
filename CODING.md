d69747ff5685ff040874a291cf7fbb560c68f979
Loïc Molinari <loic.molinari@canonical.com>
2017-04-08 16:03:13 +0200

    Added a bunch of files.

---
+++ CODING.md
@@ -0,0 +1,63 @@
+# C++ coding style
+
+Flupke follows the Qt conventions, see:
+- https://wiki.qt.io/Coding_Conventions
+- https://wiki.qt.io/Qt_Coding_Style
+
+On top of it we add the few clarifications listed below.
+
+## Header inclusion
+
+1. When including Qt or Flupke headers, always prefix with the project name. Use:
+
+```
+#include <QtCore/QMap>
+#include <FlupkeMetrics/applicationmonitor.h>
+```
+
+not
+
+```
+#include <QMap>
+#include <applicationmonitor.h>
+```
+
+2. For readability reasons and to avoid hidden dependencies, headers should be
+included in ordered sections, separated by an empty line, as follows:
+
+```
+// The project's related header (for instance here for foo.cpp).
+#include "foo.h"
+
+// C standard library headers.
+#include <sys/types.h>
+#include <unistd.h>
+
+// C++ standard library headers.
+#include <functional>
+#include <hash_map>
+#include <vector>
+
+// Qt and Flupe headers. Try to first list QtCore and QtGui headers. Private
+// headers come right after public headers of the same project.
+#include <QtCore/QDebug>
+#include <QtCore/QHash>
+#include <QtCore/private/qobject_p.h>
+#include <QtGui/QWindow>
+#include <QtGui/private/qcolor_p.h>
+#include <FlupkeMetrics/applicationmonitor.h>
+
+// Other libraries headers.
+#include <gst/gst.h>
+#include <boost/call_traits.hpp>
+
+// The project's headers.
+#include "bar.h"
+#include "baz_p.h"
+```
+
+3. For libraries, the "foo" inclusion variant must be used in C++ source files
+for the project's headers. The <Project/foo.h> and <Project/private/foo_p.h>
+inclusion variant must be used for the project's headers in private and public
+headers, wherever the header is located in the source tree. That ensures the
+inclusion of private headers by external projects won't break.
---
+++ scripts/shell.sh
@@ -0,0 +1,55 @@
+#!/bin/bash -i
+
+# Copyright © 2017 Loïc Molinari <loicm@loicm.fr>
+#
+# This file is part of Flupke.
+#
+# Flupke is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation; version 3.
+#
+# Flupke is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License along
+# with Flupke. If not, see <http://www.gnu.org/licenses/>.
+
+# This script spawns a new Bash shell with all the environment variables needed
+# to run apps based on the local source tree uninstalled. Note that qmake must
+# be run before in order to have the .build_dir.inc file generated:
+
+#  $ qmake
+#  $ ./scripts/shell.sh
+#  [Flupke] $ make
+#  [Flupke] $ qmlscene-flupke <file>
+
+NAME='Flupke'
+
+BUILD_DIR_FILENAME=`dirname ${BASH_SOURCE[0]}`/../.build_dir.inc
+if ! [ -f $BUILD_DIR_FILENAME ]
+then
+    echo "Generate makefiles with 'qmake' first!"
+    exit 1
+fi
+. $BUILD_DIR_FILENAME || exit 1
+
+export QML2_IMPORT_PATH=$BUILD_DIR/qml
+export LD_LIBRARY_PATH=$BUILD_DIR/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
+export PKG_CONFIG_PATH=$BUILD_DIR/lib/pkgconfig${PKG_CONFIG_PATH:+:$PKG_CONFIG_PATH}
+export PATH=$BUILD_DIR/bin${PATH:+:$PATH}
+
+TEMP_FILE=`mktemp -t bashrc.XXXXXXXX`
+echo source $HOME/.bashrc >> $TEMP_FILE
+echo PS1=\'[$NAME] $PS1\' >> $TEMP_FILE
+SHELL_OPTIONS="--init-file $TEMP_FILE"
+
+echo Entering $NAME shell.
+$SHELL $SHELL_OPTIONS
+echo Leaving $NAME shell. Have a nice day!
+
+if test ! -z "$TEMP_FILE"
+then
+    rm $TEMP_FILE
+fi
